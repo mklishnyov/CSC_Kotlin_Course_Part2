3e84b99c29b136c2e54379845f24995ac38b97fb
Matvey <klishnev2018@gmail.com>
2025-08-31 08:41:43 +0500

    31.08.25

--- aliasServer/task/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamService.kt
+++ aliasServer/task/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamService.kt
@@ -1,9 +1,16 @@
 package jetbrains.kotlin.course.alias.team
 
+import jetbrains.kotlin.course.alias.util.Identifier
+import jetbrains.kotlin.course.alias.util.IdentifierFactory
 import org.springframework.stereotype.Service
 
 @Service
 class TeamService {
-
-    fun generateTeamsForOneRound(teamsNumber: Int): List<Team> = TODO("Not implemented yet")
+    val identifierFactory: IdentifierFactory = IdentifierFactory(0)
+    companion object {
+        val teamsStorage: MutableMap<Identifier, Team> = mutableMapOf()
+    }
+    fun generateTeamsForOneRound(teamsNumber: Int): List<Team> {
+        
+    }
 }
